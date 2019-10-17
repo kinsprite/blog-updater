1ef1abc81989c7be47cccf02bf3417d6a51c6f58
kinsprite <qxqs11@126.com>
2019-10-18 01:14:27 +0800

    update shell

--- do-update.sh
+++ do-update.sh
@@ -4,9 +4,19 @@ baseURL=https://your-example.com/
 logfile=/var/log/blog-updater.log
 
 cd /opt/personal-blog
+curDirName=$(date +"%Y%m%dT%H%M%S")
+destDir=public/all/$curDirName
 
 echo ---- $(date) ---- >> $logfile
 
 git pull >> $logfile \
  && git submodule update >> $logfile \
- && hugo --cleanDestinationDir --baseURL $baseURL  >> $logfile
+ && hugo --cleanDestinationDir --baseURL $baseURL --destination $destDir >> $logfile
+
+if [ $? -eq 0 ]; then
+    cd /opt/personal-blog/public
+    ln -nfs ./all/$curDirName current
+
+    cd /opt/personal-blog/public/all
+    find . -mindepth 1 -name $curDirName -type d -prune -o -exec rm -rf {} +
+fi
